8ecc5543e0206a85d5e86bd2ff1da2d5eeb8eee6
Robert JF <robertojf2012@gmail.com>
2018-08-22 19:00:09 -0600

    Se crea script haciendo operaciones basicas

---
+++ Programacion Internet/practica1.js
@@ -0,0 +1,31 @@
+const args = process.argv;
+var result = 0;
+
+switch(args[3].toLowerCase()){
+
+	case 'mas':
+		var number1 = parseInt(args[2]);
+		var number2 = parseInt(args[4]);
+		result = number1+number2;
+		console.log("Resultado: "+result);
+	break;
+
+	case 'menos':
+		var number1 = parseInt(args[2]);
+		var number2 = parseInt(args[4]);
+		result = number1-number2;
+		console.log("Resultado: "+result);
+	break;
+
+	case 'entre':
+		var number1 = parseInt(args[2]);
+		var number2 = parseInt(args[4]);
+		result = number1/number2;
+		console.log("Resultado: "+result);
+	break;
+
+	default: console.log("Ingrese operador correcto");
+
+}
+
+
